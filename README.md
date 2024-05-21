a62a40c348538d0c5c3132876aa9590f5270dd40
Candice Weaver <candicenw@hotmail.co.uk>
2024-05-21 23:46:56 +0100

    finished project and set up markdown & comments for viewers

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# chatgpt-testing
+# ChatGPT Testing
+
 Just playing around learning how to implement ChatGPT using Node
+
+## To install
+
+1. ```
+    npm i
+```
+
+
+2. ```
+    npm start
+```
+
+3. Uncomment the section you want, then see the output in the terminal.
+
+## Examples
+
+1. Text to text conversation
+
+To start with, I told ChatGPT to be a helpful assistant. I then asked what the oldest English county is. Below is its output:
+
+![Text to Text Output](/output-text-to-text-short.png "Text to Text Output")
+
+2. Longer text to text conversation
+
+This example follows on from the previous one. I took an except from ChatGPT's answer and used that as an answer to the original question. I then asked where those counties can be found.
+
+![Longer Text to Text Output](/output-text-to-text-short.png "Longer Text to Text Output")
+
+I also narrowed down the answer so we only get ChatGPT's new answer returned.
+
+3. Image to text
+
+For this example, I gave ChatGPT the link to a stock photo of a dog wearing glasses and asked what the creature (the dog) is wearing.
+
+![Image to Text Output](/output-text-to-text-short.png "Image to Text Output")
+
+*Please note, I included the photo in the project's folder and left its file name in a comment about the url.*
+
+4. Multiple images to text / Image comparison
+
+For this example, I gave ChatGPT links to 2 stock photos asked what the differences between the 2 photos are.
+
+![Multiple Images to Text Output](/output-text-to-text-short.png "Multiple Images to Text Output")
+
+*Again, I included the photos in the project's folder and left the file names in comment about the urls.*
--- index.js
+++ index.js
@@ -1,5 +1,113 @@
 const OpenAI = require('openai')
 
+require('dotenv').config()
+
 const openai = new OpenAI({
-  apiKey: `${process.env.openAiAPIKey}`,
+  apiKey: process.env.OPENAI_API_KEY,
 })
+
+// // 1) Text to text conversation
+// const getText = async () => {
+//   const completion = await openai.chat.completions.create({
+//     messages: [
+//       {
+//         role: 'system',
+//         content: 'You are a helpful assistant',
+//       },
+//       {
+//         role: 'user',
+//         content: 'What is the oldest English county?',
+//       },
+//     ],
+//     model: 'gpt-4o',
+//   })
+
+//   console.log(completion.choices)
+// }
+
+// // 2) Longer text to text conversation
+// const getText = async () => {
+//   const completion = await openai.chat.completions.create({
+//     messages: [
+//       {
+//         role: 'system',
+//         content: 'You are a helpful assistant',
+//       },
+//       {
+//         role: 'user',
+//         content: 'What is the oldest English county?',
+//       },
+//       {
+//         role: 'assistant',
+//         content:
+//           'Some of the oldest established regions include Sussex, Kent, and Essex.',
+//       },
+//       {
+//         role: 'user',
+//         content: 'Where are those?',
+//       },
+//     ],
+//     model: 'gpt-4o',
+//   })
+
+//   console.log(completion.choices[0].message)
+// }
+
+// // 3) Image to text
+// const getText = async () => {
+//   const completion = await openai.chat.completions.create({
+//     messages: [
+//       {
+//         role: 'user',
+//         content: [
+//           { type: 'text', text: 'What is this creature wearing?' },
+//           {
+//             type: 'image_url',
+
+//             //dog-in-glasses.jpg
+//             image_url: { url: 'https://i.ibb.co/Vm1gXyL/dog-in-glasses.jpg' },
+//           },
+//         ],
+//       },
+//     ],
+//     model: 'gpt-4o',
+//   })
+
+//   console.log(completion.choices[0].message.content)
+// }
+
+// // 4) Multiple images to text/Comparing images
+// const getText = async () => {
+//   const completion = await openai.chat.completions.create({
+//     messages: [
+//       {
+//         role: 'user',
+//         content: [
+//           {
+//             type: 'text',
+//             text: 'What is the difference between the two images?',
+//           },
+//           {
+//             type: 'image_url',
+//             image_url: {
+//               //person-holding-lightbulb.jpg
+//               url: 'https://i.ibb.co/kH0QJZv/diego-ph-f-Iq0t-ET6llw-unsplash.jpg',
+//             },
+//           },
+//           {
+//             type: 'image_url',
+//             image_url: {
+//               //rose-in-vase.jpg
+//               url: 'https://i.ibb.co/wwjjn7n/tirza-van-dijk-c-NGUw-CEsp0-unsplash.jpg',
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//     model: 'gpt-4o',
+//   })
+
+//   console.log(completion.choices[0])
+// }
+
+getText()
